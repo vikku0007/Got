52f333eebc3dfd50f7222a7ece008bcdb3a7eed3
vikku0007 <vikash.bhardwaj@nbgspl.com>
2021-11-29 17:45:17 +0530

    changes done

--- src/Pages/Notify/Notification.js
+++ src/Pages/Notify/Notification.js
@@ -5,11 +5,6 @@ import './Notification.css';
 import Leftbar from '../Sidebar/Leftbar';
 import Rightbar from '../Sidebar/Rightbar';
 
-import Box from '@mui/material/Box';
-import Tab from '@material-ui/core/Tab';
-import { TabContext } from '@material-ui/lab';
-import TabList from '@material-ui/lab/TabList';
-import TabPanel from '@material-ui/lab/TabPanel';
 
 function Notification() {
     const [value, setValue] = React.useState('1');
@@ -26,20 +21,7 @@ function Notification() {
                             <Leftbar />
                         </div>
                         <div className="contentSection">
-                            <Box sx={{ width: '100%', typography: 'body1' }}>
-                                <TabContext value={value}>
-                                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                                    <TabList onChange={handleChange} aria-label="lab API tabs example">
-                                        <Tab label="Item One" value="1" />
-                                        <Tab label="Item Two" value="2" />
-                                        <Tab label="Item Three" value="3" />
-                                    </TabList>
-                                    </Box>
-                                    <TabPanel value="1">Item One</TabPanel>
-                                    <TabPanel value="2">Item Two</TabPanel>
-                                    <TabPanel value="3">Item Three</TabPanel>
-                                </TabContext>
-                            </Box>                 
+                                           
                         </div>                        
                         <div className="stickyPos">
                             <Rightbar />
